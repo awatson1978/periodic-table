9d545e9a351871da3589e307bd6f0c2f608167fa
Abigail Watson <awatson1978@gmail.com>
2015-05-27 21:28:06 -0500

    they all rotate!

--- client/components/singleTileDemo/singleTileDemo.js
+++ client/components/singleTileDemo/singleTileDemo.js
@@ -12,34 +12,30 @@ Template.singleTileDemo.onRendered(function() {
     rootNode
         .setSizeMode('absolute', 'absolute', 'absolute')
         .setAbsoluteSize(360, 480)
-        .setAlign(0.5, 0.5, 10)
+        .setAlign(0.15, 0.5, 10)
         .setMountPoint(0.5, 0.5)
-        .setOrigin(0.5, 0.5,10);
+        .setOrigin(0, 0, 0);
 
 
     var colors = ["Red", "Blue", "Green"];
 
-    Elements.find().fetch().forEach(function(element){
+    Elements.find().fetch().forEach(function(element, index){
       console.log("element", element);
-    });
 
-    colors.forEach(function(color, index){
-      console.log("color", color);
+      var elementNode = rootNode.addChild();
 
-      var foo = rootNode.addChild();
-
-      foo
+      elementNode
         .setSizeMode('absolute', 'absolute', 'absolute')
         .setAbsoluteSize(120, 160)
-        .setAlign(index + 0.5, 0.5,10)
-        .setMountPoint(index + 0.5, 0.5)
-        .setOrigin(0.5 + index, 0.5 + index, 10)
+        .setAlign(index * 0.5, 0.5, 10)
+        .setMountPoint( 0.5, 0.5)
+        .setOrigin(0.5, 0.5, 10)
 
 
-      var element = new Famous.DOMElement(foo, {
+      new Famous.DOMElement(elementNode, {
           tagName: 'div',
-          //content: color + "<br>" + index,
-          content: $('#sampleText').html(),
+          content: '<span class="atomicNumberText">' + element.atomicNumber + '</span><br><h1 class="center">' + element.symbol + '</h1><h5 class="center">' + element.name + '<br>' + element.atomicMass + '<br></h5>',
+          //content: $('#sampleText').html(),
           properties: {
               'height': '160px',
               'width': '120px',
@@ -62,34 +58,15 @@ Template.singleTileDemo.onRendered(function() {
       })
       .setAttribute('src', 'panel-item-ticks-hq.png');
 
+      var spinner = elementNode.addComponent({
+          onUpdate: function(time) {
+              elementNode.setRotation(0, time / 1000, 0);
+              elementNode.requestUpdateOnNextTick(spinner);
+          }
+      });
+      elementNode.requestUpdate(spinner);
+
     });
-
-
-
-    /*var element2 = new Famous.DOMElement(rootNode, {
-        tagName: 'img',
-        content: 'Famo.us Mixed Mode Examples!',
-        properties: {
-            'border':'1px solid black',
-            'background-color':'rgba(73, 160, 154, 0.160784)',
-            'color':'white',
-            'text-align':'center',
-            'cursor': 'pointer',
-            'opacity': '.99999',
-            'letter-spacing': '2px',
-            'background-repeat': 'no-repeat',
-            'background-size': '50% 50%',
-        }
-    }).setAttribute('src', 'panel-item-ticks-hq.png');*/
-
-
-    var spinner = rootNode.addComponent({
-        onUpdate: function(time) {
-            rootNode.setRotation(0, time / 1000, 0);
-            rootNode.requestUpdateOnNextTick(spinner);
-        }
-    });
-    rootNode.requestUpdate(spinner);
 
 
 });
